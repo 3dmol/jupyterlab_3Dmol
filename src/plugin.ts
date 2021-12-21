8f888bee7a812cad9caf1aff7c2887509f9b3c58
David Koes <dkoes@pitt.edu>
2021-12-21 14:04:05 -0500

    Update to newer typescript etc.

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,62 +1,58 @@
-import {
-  DocumentRegistry
-} from '@jupyterlab/docregistry'
+import { DocumentRegistry } from '@jupyterlab/docregistry'
+import { INotebookModel, NotebookPanel } from '@jupyterlab/notebook'
+import { JupyterFrontEndPlugin, JupyterFrontEnd } from '@jupyterlab/application'
+import { IDisposable, DisposableDelegate } from '@lumino/disposable'
 
-import {
-  INotebookModel,
-  NotebookPanel
-} from '@jupyterlab/notebook'
-
-import {
-  JupyterLabPlugin,
-  JupyterLab
-} from '@jupyterlab/application'
-
-import {
-  IDisposable,
-  DisposableDelegate
-} from '@phosphor/disposable'
-
-import {
-  ContextManager
-} from './manager'
-
+import { ContextManager } from './manager'
 import {  
   JSLoad_3Dmol,
   LOAD_MIME_TYPE_3Dmol
 } from './renderer'
 
 
-export
-type INBWidgetExtension = DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel>;
+export type INBWidgetExtension = DocumentRegistry.IWidgetExtension<
+  NotebookPanel,
+  INotebookModel
+>
 
+export class NBWidgetExtension implements INBWidgetExtension {
+  createNew(
+    nb: NotebookPanel,
+    context: DocumentRegistry.IContext<INotebookModel>
+  ): IDisposable {
+    const manager = new ContextManager(context)
 
-export
-class NBWidgetExtension implements INBWidgetExtension {
-  createNew(nb: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
-    let manager = new ContextManager(context);
-
-    nb.rendermime.addFactory({
+    nb.content.rendermime.addFactory({
         safe: false,
         mimeTypes: [LOAD_MIME_TYPE_3Dmol],
         createRenderer: (options) => new JSLoad_3Dmol(options)
     }, 0);
 
     return new DisposableDelegate(() => {
-        if (nb.rendermime) {
-            nb.rendermime.removeMimeType(LOAD_MIME_TYPE_3Dmol);
+        if (nb.content.rendermime) {
+            nb.content.rendermime.removeMimeType(LOAD_MIME_TYPE_3Dmol);
         }
         manager.dispose();
     });
+
+    nb.content.rendermime.addFactory(
+      {
+        safe: false,
+        mimeTypes: [LOAD_MIME_TYPE_3Dmol],
+        createRenderer: options => new JSLoad_3Dmol(options)
+      },
+      0
+    )
   }
 }
 
+
 export
-  const extension: JupyterLabPlugin<void> = {
+  const extension: JupyterFrontEndPlugin<void> = {
     id: 'jupyterlab_3Dmol',
     autoStart: true,
-    activate: (app: JupyterLab) => {
-        // this adds the Bokeh widget extension onto Notebooks specifically
+    activate: (app: JupyterFrontEnd) => {
         app.docRegistry.addWidgetExtension('Notebook', new NBWidgetExtension());
     }
 }
+
